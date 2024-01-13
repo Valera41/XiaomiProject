134da1a5190aecb4a709b416a55269df59bf7eb6
Valera41 <whesacred@gmail.com>
2024-01-13 14:52:22 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.bike__slider, .bike__slider2 , .colors').slick({
+        arrows:false,
+        dots:true,
+        fade:true,
+        autoplay:true,
+        autoplayspeed:2000,
+        
+    });
+} );
